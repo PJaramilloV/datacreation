7adb0c83ecf56f3b0d612cffb652f3eed49a2461
Cubolink <joaquin.cruz.c@ug.uchile.cl>
2024-03-28 15:20:25 -0300

    feat: support multiple breaks per object
    A refactor had to be made too.

--- preproc/degrade_cloud_bottom.py
+++ preproc/degrade_cloud_bottom.py
@@ -5,6 +5,7 @@ import subprocess
 import argparse
 import mcubes
 import os
+import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--multiprocessing', type=eval, default=True, help="set multiprocessing True/False")
@@ -21,6 +22,7 @@ parser.add_argument('--maxx', type=float, default=0, help='maximum rotation appl
 parser.add_argument('--maxy', type=float, default=0, help='maximum rotation appliable to the objects in the Y axis in both directions (in degrees)')
 parser.add_argument('--maxz', type=float, default=0, help='maximum rotation appliable to the objects in the Z axis in both directions (in degrees)')
 parser.add_argument('--seed', type=int, default=1234, help='sets numpy random seed')
+parser.add_argument('--n_breaks', type=int, default=1, help='how many broken objects per object')
 opt = parser.parse_args()
 breakage = opt.breakage
 variance = opt.variance
@@ -111,7 +113,7 @@ def rotZ(theta):
                      [np.sin(theta), np.cos(theta), 0],
                      [0, 0, 1]])
 
-def make_rotation_matrix(points):
+def make_rotation_matrix(points, rotation_number):
     """Uses points' data for random rotation seed. Returns points, randomState and rotation matrix"""
     theta_x, theta_y, theta_z = opt.maxx, opt.maxy, opt.maxz
     thetas = [th * np.pi /180 for th in [theta_x, theta_y, theta_z]]
@@ -119,7 +121,7 @@ def make_rotation_matrix(points):
     # np.random.random doesn't work randomly through threads
     #   solution: make a random state taking a coordinate sum from the 
     #             coordinates of a point of the object shape
-    rs = np.random.RandomState(int(sum( points[points.shape[0]//2]  )% (1<<30) ))
+    rs = np.random.RandomState(int(sum( points[points.shape[0]//2], rotation_number)% (1<<30) ))
     for th, rotation in zip(thetas, [rotX, rotY, rotZ]):
         if not th:
             continue
@@ -145,49 +147,57 @@ def remove_bottom_points(point_cloud, rand_state=np.random.RandomState(0)):
     broken, piece = point_cloud[point_cloud[:, 1] > full_breakoff], point_cloud[point_cloud[:, 1] <= full_breakoff]
     return broken, piece
 
-def break_piece(npy_path):
+def break_piece(npy_path, break_number):
     pc = np.load(npy_path)
-    pc, rs, rot_mat = make_rotation_matrix(pc)
+    pc, rs, rot_mat = make_rotation_matrix(pc, break_number)
     pc = rotate(pc, rot_mat)
     broken_cloud, piece_cloud = remove_bottom_points(pc, rs)
     broken_cloud = rotate(broken_cloud, rot_mat.T)
     piece_cloud = rotate(piece_cloud, rot_mat.T)
     return broken_cloud, piece_cloud
 
-def break_and_save(npy_path):
-    broken, piece = break_piece(npy_path)
-
+def break_and_save(npy_path, n):
     broken_npy_path = npy_path.replace('complete', 'broken')
     repair_npy_path = npy_path.replace('complete', 'repair')
 
     os.makedirs(os.path.dirname(broken_npy_path), exist_ok=True)
     os.makedirs(os.path.dirname(repair_npy_path), exist_ok=True)
 
-    np.save(broken_npy_path, broken)
-    np.save(repair_npy_path, piece)
+    for i in range(n):
+        broken, piece = break_piece(npy_path, i)
+        
+        _, ext = os.path.splitext(npy_path)
 
-    if len(broken.shape) != 3:
-        if broken.shape[0] == 3:
-            broken = broken.T
-        if broken.shape[1] == 3:
-            broken = volume_from_points(broken)
-    if len(piece.shape) != 3:
-        if piece.shape[0] == 3:
-            piece = piece.T
-        if piece.shape[1] == 3:
-            piece = volume_from_points(piece)
+        serial_name = f"_{i}{ext}"
+        broken_npy_path_i = broken_npy_path.replace(ext, serial_name)
+        repair_npy_path_i = repair_npy_path.replace(ext, serial_name)
+        complete_npy_path_i = npy_path.replace(ext, serial_name)
+        np.save(broken_npy_path_i, broken)
+        np.save(repair_npy_path_i, piece)
+        shutil.copy(npy_path, complete_npy_path_i)
+        
+        save_obj(broken_npy_path_i, broken)
+        save_obj(repair_npy_path_i, piece)
+
+
+def parallel_break_and_save(args):
+    return break_and_save(args[0], args[1])
+
+
+def save_obj(npy_path, points):
+    if len(points.shape) != 3:
+        if points.shape[0] == 3:
+            points = points.T
+        if points.shape[1] == 3:
+            points = volume_from_points(points)
     
-    if len(broken.shape) != 3 or len(piece.shape) != 3:
-        raise ValueError(f"Check array dimensions, should be (x,x,x). Shapes (3,x) and (x,3) are supported but this is {broken.shape} and {piece.shape}")
+    if len(points.shape) != 3:
+        raise ValueError(f"Check array dimensions, should be (x,x,x). Shapes (3,x) and (x,3) are supported but this is {points.shape} and {piece.shape}")
     
-    broken_v, broken_f = mcubes.marching_cubes(broken, 0)
-    repair_v, repair_f = mcubes.marching_cubes(piece, 0)
+    v, f = mcubes.marching_cubes(points, 0)
+    obj_path = npy_path.replace('.npy','.obj')
 
-    broken_obj_path = broken_npy_path.replace('.npy','.obj')
-    repair_obj_path = repair_npy_path.replace('.npy','.obj')
-
-    mcubes.export_obj(broken_v, broken_f, broken_obj_path)
-    mcubes.export_obj(repair_v, repair_f, repair_obj_path)
+    mcubes.export_obj(v, f, obj_path)
     
 
 
@@ -229,7 +239,6 @@ if __name__ == '__main__':
     threads = opt.threads
     parallel = opt.multiprocessing
     directory = os.path.join(data_dir, dataset, 'complete')
-
     counter = 0
     database = []
     for root, dirs, files in os.walk(directory, topdown=False):
@@ -242,14 +251,17 @@ if __name__ == '__main__':
                     datapoint = os.path.join(root, filename)
                     database.append(datapoint)
     print(f"Found {len(database) }/{counter} files unprocessed")
+    
+    n_breaks = opt.n_breaks
     if parallel:
+        n_breaks = [n_breaks] * len(database)
         pool = multiprocessing.Pool(threads)
         try:
-            for _ in tqdm(pool.imap_unordered(break_and_save, database), total=len(database)):
+            for _ in tqdm(pool.imap_unordered(parallel_break_and_save, zip(database, n_breaks)), total=len(database)):
                 pass
         except KeyboardInterrupt:
             exit()
         pool.close()
     else:
         for data in tqdm(database):
-            break_and_save(data)
+            break_and_save(data, n_breaks)
